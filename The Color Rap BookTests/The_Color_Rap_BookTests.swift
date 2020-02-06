9885f5faa8ddb21aa2a3004b983f43312bcda442
Yuxin <yuxinliuhubert@gmail.com>
2020-02-06 16:43:58 -0500

    Initial Commit

---
+++ The Color Rap BookTests/The_Color_Rap_BookTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Color_Rap_BookTests.swift
+//  The Color Rap BookTests
+//
+//  Created by 7744 on 2/6/20.
+//  Copyright © 2020 7744. All rights reserved.
+//
+
+import XCTest
+@testable import The_Color_Rap_Book
+
+class The_Color_Rap_BookTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
